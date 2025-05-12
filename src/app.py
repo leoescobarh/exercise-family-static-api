70f51ba0a7add7b1fb67f4ce0ba3308cebe70ba5
leoescobarh <ldescobarherrera@gmail.com>
2025-05-12 17:22:56 +0000

    api estatica con flask para familia

--- src/app.py
+++ src/app.py
@@ -30,14 +30,35 @@ def sitemap():
 
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
+def get_all_members():
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    return jsonify(members), 200
+
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404
+
+@app.route('/members', methods=['POST'])
+def add_member():
+    member = request.get_json()
+    if not member or not member.get("first_name") or not member.get("age") or not member.get("lucky_numbers"):
+        return jsonify({"error": "Invalid member data"}), 400
+    new_member = jackson_family.add_member(member)
+    return jsonify(new_member), 200
 
 
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        jackson_family.delete_member(member_id)
+        return jsonify({"done": True}), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404
 
 # This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -10,36 +10,30 @@ class FamilyStructure:
         self.last_name = last_name
         self._next_id = 1
         self._members = [
-            {
-                "id": self._generate_id(),
-                "first_name": "John",
-                "last_name": last_name,
-                "age": 33,
-                "lucky_numbers": [7, 13, 22]
-            }
+            {"id": self._generate_id(), "first_name": "John", "last_name": last_name, "age": 33, "lucky_numbers": [7, 13, 22]},
+            {"id": self._generate_id(), "first_name": "Jane", "last_name": last_name, "age": 35, "lucky_numbers": [10, 14, 3]},
+            {"id": self._generate_id(), "first_name": "Jimmy", "last_name": last_name, "age": 5, "lucky_numbers": [1]}
         ]
 
-    # This method generates a unique incremental ID
     def _generate_id(self):
         generated_id = self._next_id
         self._next_id += 1
         return generated_id
 
     def add_member(self, member):
-        ## You have to implement this method
-        ## Append the member to the list of _members
-        pass
+        if "id" not in member:
+            member["id"] = self._generate_id()
+        member["last_name"] = self.last_name
+        self._members.append(member)
+        return member
 
     def delete_member(self, id):
-        ## You have to implement this method
-        ## Loop the list and delete the member with the given id
-        pass
+        self._members = [m for m in self._members if m["id"] != id]
+        return True
 
     def get_member(self, id):
-        ## You have to implement this method
-        ## Loop all the members and return the one with the given id
-        pass
+        member = next((m for m in self._members if m["id"] == id), None)
+        return member
 
-    # This method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
